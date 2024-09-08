f17af95e7c157407d76edab107f2e38a4f4a9c93
Hritik Yadav <144812620+Azmodeus07@users.noreply.github.com>
2024-09-08 22:52:50 +0530

    Currency changer app
    Made a currency changer app and learned about useId hook and make cutom hooks, used currency api to get the data

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     let pass = "";
     let str ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if(numberAllowed) str += "0123456789";
-    if(charAllowed) str += "!@#$,.?";
+    if(charAllowed) str += "@$@$@$";
 
     for (let i = 1; i <= length; i++) {
       let char  = Math.floor(Math.random()*str.length +1); 
---
+++ 06currencyConverter/src/components/index.js
@@ -0,0 +1,5 @@
+import InputBox from "./InputBox";
+
+export {InputBox}
+
+//used in heavy projects when there are lot of components
