2a5716904a87fc389ee5b93fd481d699013354dd
Hritik Yadav <144812620+Azmodeus07@users.noreply.github.com>
2024-09-10 23:27:24 +0530

    Theme Changer done
    applied theme changer and new way to route when there are less routes i.e efficient use of usecontext api

--- 07reactRouter/src/components/User/User.jsx
+++ 07reactRouter/src/components/User/User.jsx
@@ -4,7 +4,11 @@ import { useParams } from 'react-router-dom'
 function User() {
     const {userid} =useParams()
   return (
-    <div className='bg-gray-600 m-5  text-white text-3xl flex justify-center py-10'> User :  {userid}</div>
+    <>
+    <div className='bg-gray-600 m-5  text-white text-3xl flex justify-center py-10'> User :  {userid}
+    </div>
+    {/* <button className=' m-4 px-3 py-2 text-2xl bg-red-700 text-white rounded-md cursor-pointer'>Subscribe</button> */}
+    </>
   )
 }
 
