e618003056b93cb45577d78cea839a35f1e448c0
Hritik Yadav <144812620+Azmodeus07@users.noreply.github.com>
2024-09-15 23:07:38 +0530

    Intialized the Appwrite related content

---
+++ 12megaBlog/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+    <>
+      <h1> Let's start the Blog making  </h1>
+    </>
+  )
+}
+
+export default App
