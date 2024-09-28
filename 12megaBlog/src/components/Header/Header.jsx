883cf6f0949ed7e3a2c17d53f385294140afcc4b
Hritik Yadav <144812620+Azmodeus07@users.noreply.github.com>
2024-09-28 16:05:51 +0530

    minor changes

--- 12megaBlog/src/components/Header/Header.jsx
+++ 12megaBlog/src/components/Header/Header.jsx
@@ -53,7 +53,7 @@ function Header() {
               <li key={item.name}>
                 <button
                 onClick={() => navigate(item.slug)}
-                className='inline-bock px-6 py-2 duration-200 hover:bg-slate-500 rounded-full'
+                className='inline-block px-6 py-2 duration-200 hover:bg-slate-500 rounded-full'
                 >{item.name}</button>
               </li>
             ) : null
